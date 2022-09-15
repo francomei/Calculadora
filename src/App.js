6a822d170c698653b0956aeb7ab8b5938ac3699b
unknown <francomeinardo13@gmail.com>
2022-09-15 16:31:32 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import Boton from './componentes/boton';
+import Pantalla from './componentes/pantalla';
+import BotonClear from './componentes/botonClear';
+import { useState } from "react"
+import { evaluate } from "mathjs"
 
 function App() {
+
+  const [input, setImput] = useState("");
+
+  const agregarImput = value => {
+    setImput(input + value)
+  };
+
+  const calcularResultado = () => {
+    if(input){
+      setImput(evaluate(input))
+    }else{
+      alert("Por favor ingrese valores para realizar los cálculos.")
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='contenedor-calculadora'>
+        <Pantalla input={input} />
+        <div className='fila'>
+          <Boton manejarClick={agregarImput}>1</Boton>
+          <Boton manejarClick={agregarImput}>2</Boton>
+          <Boton manejarClick={agregarImput}>3</Boton>
+          <Boton manejarClick={agregarImput}>+</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClick={agregarImput}>4</Boton>
+          <Boton manejarClick={agregarImput}>5</Boton>
+          <Boton manejarClick={agregarImput}>6</Boton>
+          <Boton manejarClick={agregarImput}>-</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClick={agregarImput}>7</Boton>
+          <Boton manejarClick={agregarImput}>8</Boton>
+          <Boton manejarClick={agregarImput}>9</Boton>
+          <Boton manejarClick={agregarImput}>*</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClick={calcularResultado}>=</Boton>
+          <Boton manejarClick={agregarImput}>0</Boton>
+          <Boton manejarClick={agregarImput}>.</Boton>
+          <Boton manejarClick={agregarImput}>/</Boton>
+        </div>
+        <div className='fila'>
+          <BotonClear manejarClear={() => setImput("")}>
+            Clear
+          </BotonClear>
+        </div>
+        
+      </div>
     </div>
   );
 }
